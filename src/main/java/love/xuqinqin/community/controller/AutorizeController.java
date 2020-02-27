a77ff2c21ea1dc6dbdd03a04311296b4ba13044e
Guy-Fight <1025311173@qq.com>
2020-02-27 12:11:47 +0800

    change properties file

--- src/main/java/love/xuqinqin/community/controller/AutorizeController.java
+++ src/main/java/love/xuqinqin/community/controller/AutorizeController.java
@@ -4,6 +4,7 @@ import love.xuqinqin.community.dto.AccessTokenDTO;
 import love.xuqinqin.community.dto.GithubUser;
 import love.xuqinqin.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,15 +21,24 @@ public class AutorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientID;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.url}")
+    private String redirect;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
                            HttpSession session){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("abc5b445342d6b4a72fb");
-        accessTokenDTO.setClient_secret("9296704921ae30a847c7600d6e052ece24386049");
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirect);
         accessTokenDTO.setState(state);
         //根据AccessTokenDTO对象转化的json作为请求体post请求，响应得到token码；
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
