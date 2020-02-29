d9038a22a65efae5737d0eb9f8fdfb584367878d
Guy-Fight <1025311173@qq.com>
2020-02-29 22:59:25 +0800

    import lombok

--- src/main/java/love/xuqinqin/community/model/User.java
+++ src/main/java/love/xuqinqin/community/model/User.java
@@ -1,9 +1,12 @@
 package love.xuqinqin.community.model;
 
+import lombok.Data;
+
 /**
  * @Author FGuy
  * @Date 2020/2/27 15:08
  */
+@Data
 public class User {
 
     private Integer id;
@@ -13,88 +16,6 @@ public class User {
     private Long gmtCreate;
     private Long gmtModified;
     private String bio;
+    private String avatarUrl;
 
-    public User(){
-
-    }
-
-    public User(Integer id,String name,String accountId,String token,Long gmtCreate,Long gmtModified,String bio){
-        this.id = id;
-        this.name = name;
-        this.accountId = accountId;
-        this.token = token;
-        this.gmtCreate = gmtCreate;
-        this.gmtModified = gmtModified;
-        this.bio = bio;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Long gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Long getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Long gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", accountId='" + accountId + '\'' +
-                ", token='" + token + '\'' +
-                ", gmtCreate=" + gmtCreate +
-                ", gmtModified=" + gmtModified +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
 }
